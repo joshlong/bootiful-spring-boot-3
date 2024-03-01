200e9dc02bd25ee6a0335a6c695d43f4c379f50f
Josh Long <josh@joshlong.com>
2024-02-29 20:33:55 -0800

    m0ar

--- service/src/main/java/com/example/service/ServiceApplication.java
+++ service/src/main/java/com/example/service/ServiceApplication.java
@@ -1,13 +1,36 @@
 package com.example.service;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.io.FileInputStream;
 
 @SpringBootApplication
 public class ServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceApplication.class, args);
+	}
+
+}
+
+class IoExample {
+
+	@Bean
+	ApplicationRunner io() {
+		return args -> {
+
+			try (var is = new FileInputStream(".");) {
+				var next = -1;
+				while ((next = is.read()) != -1)
+					next = is.read();
+				// do something with read
+			}
+
+		};
+
 	}
 
 }
--- service/src/main/java/com/example/service/Threads.java
+++ service/src/main/java/com/example/service/Threads.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.ArrayList;
+import java.util.Set;
 import java.util.concurrent.ConcurrentSkipListSet;
 
 @Configuration
@@ -14,61 +15,34 @@ class Threads {
 	ApplicationRunner demo() {
 		return args -> {
 
+			// store all 1,000 threads
 			var threads = new ArrayList<Thread>();
 
+			// dedupe elements with Set<T>
 			var names = new ConcurrentSkipListSet<String>();
 
 			// merci José Paumard d'Oracle
-
 			for (var i = 0; i < 1000; i++) {
 				var first = 0 == i;
-				threads.add(Thread.ofVirtual().unstarted(() -> {
-					if (first)
-						names.add(Thread.currentThread().toString());
-					try {
-						Thread.sleep(100);
-					}
-					catch (InterruptedException e) {
-						throw new RuntimeException(e);
-					}
-
-					if (first)
-						names.add(Thread.currentThread().toString());
-					try {
-						Thread.sleep(100);
-					}
-					catch (InterruptedException e) {
-						throw new RuntimeException(e);
-					}
-
-					if (first)
-						names.add(Thread.currentThread().toString());
-					try {
-						Thread.sleep(100);
-					}
-					catch (InterruptedException e) {
-						throw new RuntimeException(e);
-					}
-
-					if (first)
-						names.add(Thread.currentThread().toString());
-					try {
-						Thread.sleep(100);
-					}
-					catch (InterruptedException e) {
-						throw new RuntimeException(e);
-					}
-				}));
+				threads.add(Thread.ofPlatform().unstarted(() -> run(first, names)));
 			}
 
-			for (var t : threads)
-				t.start();
+			for (var t : threads) t.start();
 
-			for (var t : threads)
-				t.join();
+			for (var t : threads) t.join();
 
 			System.out.println(names);
 		};
+	}
+
+	private static void run(boolean first, Set<String> names) {
+		if (first)
+			names.add(Thread.currentThread().toString());
+		try {
+			Thread.sleep(100);
+		} catch (InterruptedException e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 }
