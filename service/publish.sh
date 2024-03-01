5b981e3a12f8093f2347f4bb832ba025f785bfc3
Josh Long <josh@joshlong.com>
2024-02-29 22:22:47 -0800

    .pdf and .md

--- service/publish.sh
+++ service/publish.sh
@@ -1,13 +1,22 @@
 # First, convert Asciidoctor to DocBook
 
+OUTPUT=output
 gem install asciidoctor   || echo "asciidoctor is already installed"
-gem install asciidoctor-pdf --pre || echo "asciidoctor-pdf is already installed"
+gem install asciidoctor-pdf || echo "asciidoctor-pdf is already installed"
 brew install pandoc || echo "pandoc is already installed"
 
+mkdir -p $OUTPUT
 
-cp -r images publish
+cp -r images $OUTPUT
 
-## markdown first
-gem exec asciidoctor -b docbook README.adoc -o publish/file.xml
-pandoc -f docbook -t markdown publish/file.xml -o publish/README.md
+### markdown first
+gem exec asciidoctor -b docbook README.adoc -o $OUTPUT/file.xml
+pandoc -f docbook -t markdown $OUTPUT/file.xml -o $OUTPUT/README.md
 
+RUBY_PATH=`dirname  $( which ruby )`/..
+RUBY_PATH="$( cd  $RUBY_PATH && pwd )"
+ED="$( gem env | grep "EXECUTABLE DIRECTORY:" | cut -f 2 -d : )"
+APDF=`find $ED -iname "*asciidoctor-pdf"`
+echo $APDF
+
+$APDF README.adoc -o $OUTPUT/README.pdf
