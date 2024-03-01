bd63b311382f275e05d1142bfa2d7ac47718a2fd
Josh Long <josh@joshlong.com>
2024-02-29 21:46:50 -0800

    up

--- service/publish.sh
+++ service/publish.sh
@@ -5,10 +5,9 @@ gem install asciidoctor-pdf --pre || echo "asciidoctor-pdf is already installed"
 brew install pandoc || echo "pandoc is already installed"
 
 
-asciidoctor -b docbook README.adoc -o publish/file.xml
-# Then, convert DocBook to Markdown
 cp -r images publish
+
+## markdown first
+gem exec asciidoctor -b docbook README.adoc -o publish/file.xml
 pandoc -f docbook -t markdown publish/file.xml -o publish/README.md
 
-#pandoc -f docbook -t markdown publish/file.xml -o publish/README.md
-#markdown publish/README.md  > publish/README.html
